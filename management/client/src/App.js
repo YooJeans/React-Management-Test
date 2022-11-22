005c18427e158f7bbab81a11a2952b15dafe201c
YooJeans <tpfldk358@naver.com>
2022-11-22 23:54:09 +0900

    Add Get Customers REST API Module

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -19,7 +19,7 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
+});
 */
 /*
 function App() {
@@ -44,34 +44,25 @@ function App() {
 }
 */
 
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '오유진',
-    'birthday': '011122',
-    'gender': '여자',
-    'job': '대학생'
-  },
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '나동빈',
-    'birthday': '961222',
-    'gender': '남자',
-    'job': '개발자'
-  },
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '홍길동',
-    'birthday': '960305',
-    'gender': '남자',
-    'job': '디자이너'
-  }
-]
 
 class App extends Component {
+  
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     // const { classes } = this.props;
     return (
@@ -88,7 +79,8 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);})}
+            {this.state.customers ? this.state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
